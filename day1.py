d7f3b30b93cf4b61a50908129b95b4f72370acba
vishwajeetsharma <vishwajeetsharma9821593401@gmail.com>
2021-12-01 12:11:05 +0530

    through vim

---
+++ day1.py
@@ -0,0 +1,42 @@
+
+# the below code is a python code
+
+a = 15.25
+
+# premitive data types
+
+# Integer
+# float
+# strings
+# boolean
+
+# array
+# dict
+
+list1 = [
+    [1, 1, 2, 4],
+    ["jeetus", "prashants", "nitins", "rahuls"]
+]
+print(list1[0])
+
+# dict1 = {
+#     "name":"jeetu",
+#     "class":5,    
+# }
+# print(dict1)
+
+a = 1
+b = 2.67
+c = "string"
+
+d = False
+e = [1, 1, 2, 4]
+f = {"name":"jeetu", "class":5,}
+
+print(a)
+print(b)
+print(c)
+
+print(a)
+print(a)
+print(a)
